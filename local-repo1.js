c89370f1ec5d306d230c5620207a9f9e137c735f
Raj <hm.l1.repo@gmail.com>
2021-01-16 18:39:06 +0530

    index & loca-repo1 files added

---
+++ local-repo1.js
@@ -0,0 +1,5 @@
+let headerDiv = document.querySelector('#header')
+let contentDiv = document.querySelector('#content');
+let footerDiv = document.querySelector('#footer');
+
+headerDiv.innerHTML = "This is coming from local-repo1 file.."
